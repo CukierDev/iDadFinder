5b6c383e2ef0dfc6e54a69b94ba304b1ca170db5
CukierDev <101931714+CukierDev@users.noreply.github.com>
2023-05-24 10:31:20 +0200

    Add project files.

---
+++ iDadFinder/Form1.cs
@@ -0,0 +1,16 @@
+namespace PrzywolywaczStarego
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Your dad's location: Heaven", "Your dad's location");
+        }
+
+    }
+}
